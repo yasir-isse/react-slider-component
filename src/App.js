4a9a304aa84c73cc9c503ccd1c960cca644900f8
Yasir <code.yasir@gmail.com>
2022-04-12 06:18:46 +0300

    add slider logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  // reset index and prevent index out of range error
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+  }, [index, people]);
+
+  // register a setInterval with a cleanup function
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+
+    return () => clearInterval(slider);
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> reviews
+        </h2>
+      </div>
+
+      <div className="section-center">
+        {/* iterate through the people's array and return all elements */}
+        {people.map((person, personIndex) => {
+          const { id, name, title, quote, image } = person;
+
+          // assign class names to elemnts depending on their index
+          let position = "nextSlide";
+          if (index === personIndex) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt="name" className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
